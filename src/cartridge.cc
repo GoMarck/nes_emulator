3f78857221c6aa52fe44835ed79091a5eac18ff4
Marck <1412354149@qq.com>
2023-08-20 22:42:17 +0800

    [bugfix] Fix cartridge bug

--- src/cartridge.cc
+++ src/cartridge.cc
@@ -1,5 +1,7 @@
 #include "nes/cartridge.h"
 
+#include <glog/logging.h>
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -13,13 +15,13 @@
 namespace nes {
 
 /// Header size is 16 bytes.
-constexpr size_t HEADER_SIZE = 2 << 4;
+constexpr size_t HEADER_SIZE = 1 << 4;
 /// Trainer size is 512 bytes.
-constexpr size_t TRAINER_SIZE = 2 << 9;
+constexpr size_t TRAINER_SIZE = 1 << 9;
 /// PRG-ROM size is 16 KB.
-constexpr size_t PRG_ROM_SIZE = 2 << 13;
+constexpr size_t PRG_ROM_SIZE = 1 << 14;
 /// CHR-ROM size is 8 KB.
-constexpr size_t CHR_ROM_SIZE = 2 << 12;
+constexpr size_t CHR_ROM_SIZE = 1 << 13;
 
 Cartridge::Cartridge(std::string path) : path_(std::move(path)) {}
 
@@ -29,37 +31,38 @@ bool Cartridge::Init() {
   auto raii = std::unique_ptr<char[]>(absoulte_path);
   RETURN_FALSE_REPENT_IF(
       realpath(path_.c_str(), absoulte_path) == nullptr,
-      "parse file real path failed: " + std::string(strerror(errno)));
+      "Parse file real path failed: " + std::string(strerror(errno)));
   path_ = std::string(absoulte_path);
 
-  FILE *file = fopen(path_.c_str(), "rb");
+  FILE *fp = fopen(path_.c_str(), "rb");
   RETURN_FALSE_REPENT_IF(
-      file == nullptr, "Failed to open file: " + std::string(strerror(errno)));
+      fp == nullptr, "Failed to open fp: " + std::string(strerror(errno)));
 
-  fseek(file, 0L, SEEK_END);
-  auto size = ftell(file);
-  fseek(file, 0L, SEEK_SET);
+  fseek(fp, 0L, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
   if (size <= HEADER_SIZE) {
-    NES_LOG(ERROR) << "Invalid file format!";
-    fclose(file);
+    NES_LOG(ERROR) << "Invalid fp format!";
+    fclose(fp);
     return false;
   }
 
   std::vector<Byte> contents;
   contents.reserve(size);
-  if (!ReadRomFile(file, contents.data(), size)) {
-    NES_LOG(ERROR) << "Read rom file failed!";
-    fclose(file);
+  if (!ReadRomFile(fp, contents.data(), size)) {
+    NES_LOG(ERROR) << "Read rom fp failed!";
+    fclose(fp);
     return false;
   }
-  fclose(file);
+  fclose(fp);
 
   RETURN_FALSE_IF(!ParseHeader(contents.data()));
 
   size_t curr_size = HEADER_SIZE;
   Byte *curr_data = contents.data() + HEADER_SIZE;
   if (exist_trainer_) {
-    RETURN_FALSE_REPENT_IF(curr_size + TRAINER_SIZE <= size, "Not enough size for trainer");
+    RETURN_FALSE_REPENT_IF(curr_size + TRAINER_SIZE > size,
+                           "Not enough size for trainer");
     trainer_.resize(TRAINER_SIZE, 0);
     memcpy(trainer_.data(), curr_data, TRAINER_SIZE);
     curr_data += TRAINER_SIZE;
@@ -67,7 +70,8 @@ bool Cartridge::Init() {
   }
 
   for (uint8_t i = 0; i < program_rom_bank_number_; ++i) {
-    RETURN_FALSE_REPENT_IF(curr_size + PRG_ROM_SIZE <= size, "Not enough size for PRG-ROM");
+    RETURN_FALSE_REPENT_IF(curr_size + PRG_ROM_SIZE > size,
+                           "Not enough size for PRG-ROM");
     std::vector<Byte> program_rom(PRG_ROM_SIZE, 0);
     memcpy(program_rom.data(), curr_data, PRG_ROM_SIZE);
     program_data_.emplace_back(std::move(program_rom));
@@ -76,11 +80,13 @@ bool Cartridge::Init() {
   }
 
   for (uint8_t i = 0; i < character_rom_bank_number_; ++i) {
-    
+    RETURN_FALSE_REPENT_IF(curr_size + CHR_ROM_SIZE > size,
+                           "Not enough size for CHR-ROM");
     std::vector<Byte> character_rom(CHR_ROM_SIZE, 0);
     memcpy(character_rom.data(), curr_data, CHR_ROM_SIZE);
     character_data_.emplace_back(std::move(character_rom));
     curr_data += CHR_ROM_SIZE;
+    curr_size += CHR_ROM_SIZE;
   }
   return true;
 }
